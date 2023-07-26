c96645efeee3eefc75da281d62a870361e83aec5
Lily <lsmgoodchild@icloud.com>
2023-07-26 14:52:23 +0100

    Initial Commit

---
+++ stemInspiration/stemInspirationApp.swift
@@ -0,0 +1,17 @@
+//
+//  stemInspirationApp.swift
+//  stemInspiration
+//
+//  Created by scholar on 26/07/2023.
+//
+
+import SwiftUI
+
+@main
+struct stemInspirationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
